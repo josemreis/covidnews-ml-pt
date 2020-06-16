b890919877b3612cf0e50f91b7d62528374c75b2
José <31286135+josemreis@users.noreply.github.com>
2020-06-16 13:17:55 +0100

    Update covidtrain.sh

--- covidtrain.sh
+++ covidtrain.sh
@@ -1,8 +1,6 @@
 ## set working_dir
 cd '/home/jmr/Dropbox/Current projects/other_projects/covidnews-ml-pt' 
 LOGFILE="covidtrain.log"
-# check if connected to wifi
-wget -q --tries=10 --timeout=20 --spider http://google.com
 
 notify-send "Running covidtrain.sh"
 
